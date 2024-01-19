6991d89bac6ed9c1d6b75a0942b3cc6cb8182016
Kris Hamade <kris.hamade@hamy.dev>
2024-01-19 10:14:04 -0500

    Updating Front End WebSockets

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById('closure-container');
     const loader = document.getElementById('loader');
-    const ws = new WebSocket('wss://snow.hamy.cloud:8985');
+    const ws = new WebSocket('wss://snow.hamy.cloud');
     const statusDiv = document.getElementById('status');
     const userCountDiv = document.getElementById('user-count');
     const isdSelector = document.getElementById('isd-selector');
